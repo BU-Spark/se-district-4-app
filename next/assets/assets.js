3c2f7ba4393c21d99290ff6fd6d8b657c6cd869c
PranshuSwaroop15 <swarooppranshu@gmail.com>
2025-04-17 22:26:05 -0400

    resource page

---
+++ next/assets/assets.js
@@ -0,0 +1,50 @@
+// Import images
+import accessibility from './accessibility.png';
+import alert from './alert.png';
+import alertRed from './alert_red.png';
+import bIsForBoston from './b_is_for_boston.png';
+import bos311Icon from './bos_311_icon 1.png';
+import building from './building.png';
+import cityCouncil from './city_councle.png'; // Note: The filename is 'city_councle.png' — consider renaming to fix the typo
+import communityCenter from './community_centers.png';
+import info from './info.png';
+import info1 from './info-1.png';
+import location from './location.png';
+import people from './people.png';
+import playground from './playground.png';
+import policeLogo from './police_logo 1.png'; // Consider renaming file to remove the space
+import schools from './schools.png';
+import stateOffices from './state_offices.png';
+import t from './t.png';
+import teenCenter from './teen_center.png';
+import trashRecycle from './trash_and_recycling.png';
+import vendor from './vendor.png';
+import veteransServicesLogo from './veterans_services_logo 1.png';
+import wellness from './wellenss.png'; // Consider renaming to fix spelling: "wellness.png"
+import house from './house.jpeg'
+// Export images as an object
+export const assets = {
+    accessibility,
+    alert,
+    alertRed,
+    bIsForBoston,
+    bos311Icon,
+    building,
+    cityCouncil,
+    communityCenter,
+    info,
+    info1,
+    location,
+    people,
+    playground,
+    policeLogo,
+    schools,
+    stateOffices,
+    t,
+    teenCenter,
+    trashRecycle,
+    vendor,
+    veteransServicesLogo,
+    wellness,
+    house,
+};
