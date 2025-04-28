a5d5bb6116f5bedf2e6d58467b75fbbb4b472521
Minseok Lee <mslee300@bu.edu>
2025-04-28 00:09:20 -0400

    Remove all conflict markers, debug, and install swiper

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,45 +1,19 @@
-
 import { Header } from "@/app/components/Header";
 import { HeroSection } from "@/app/components/HeroSection";
 import { QuickLinks } from "@/app/components/QuickLinks";
 import { UpcomingEvents } from "@/app/components/UpcomingEvents";
 import Dfour from "./components/Dfour";
-
-<!-- <<<<<<< resource-page -->
 import ResourcePage from "@/resources/ResourcePage";
-
 
 export default function HomePage() {
   return (
-
     <main className="min-h-screen w-full overflow-x-hidden">
       <Header />
       <HeroSection />
       <QuickLinks />
       <UpcomingEvents />
       <Dfour />
-    </main>
-
-    <main >
       <ResourcePage />
-    </main>
-
-
-  );
-}
-
-import { Header } from "@/app/components/Header";
-import { HeroSection } from "@/app/components/HeroSection";
-import { QuickLinks } from "@/app/components/QuickLinks";
-import { UpcomingEvents } from "@/app/components/UpcomingEvents";
-
-export default function HomePage() {
-  return (
-    <main className="min-h-screen w-full overflow-x-hidden">
-      <Header />
-      <HeroSection />
-      <QuickLinks />
-      <UpcomingEvents />
     </main>
   );
 }
--- next/next.config.ts
+++ next/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 
-// <<<<<<< resource-page
-  /* config options here */
-
   images: {
     remotePatterns: [
       {
